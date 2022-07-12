008bd74c312777fad5acb1d4e503a3baa5c6c034
Flávia Bulad <flavia.bulad@gmail.com>
2022-07-12 19:21:14 -0300

    chore: setting up routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import chalk from "chalk";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import router from "./routes/index.js";
+
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import choiceRouter from "./choiceRouter.js";
+import pollRouter from "./pollRouter.js";
+
+const router = Router();
+
+router.use(choiceRouter);
+router.use(pollRouter);
+
+export default router;
